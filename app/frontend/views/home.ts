81ec6744108319d674b8c7894176843f468d34bb
Abderrahmane <aisdaoun@student.1337.ma>
2025-09-20 16:08:27 +0100

    Save local modifications to ft_transcendence before merging

--- app/frontend/views/home.ts
+++ app/frontend/views/home.ts
@@ -21,24 +21,3 @@ const HomeViewStaticPart: string = `
 	2025 — ft_transcendence Inc.
 </footer>
 `;
-
-// const old = `
-// <nav class="flex-container" style="grid-area: nav;">
-// 	<a class="flex-item anchor-nav" href="/" onclick="route()">home</a>
-// 	<a class="flex-item anchor-nav" href="/friends" onclick="route()">friends</a>
-// 	<a class="flex-item anchor-nav" href="/profile" onclick="route()">profile</a>
-// 	<a class="flex-item anchor-nav" href="/settings" onclick="route()">settings</a>
-// </nav>
-// <main id="main-views" style="grid-area: main; display: flex; flex-direction: column; align-items: center;">
-// 	<a class="anchor-main" style="margin-top: 125px;" href="/game" onclick="route()">play</a>
-// 	<a class="anchor-main chat" href="/chat" onclick="route()">chat</a>
-
-// 	normal text
-// 	<a>link text</a>
-// 	<button>button text</button>
-	
-// </main>
-// <footer style="grid-area: footer;">
-// 	2025 — ft_transcendence Inc.
-// </footer>
-// `;
