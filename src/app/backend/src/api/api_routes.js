468a1867aaf1d689b96b4b124753763cae104020
Your Name <you@example.com>
2025-09-25 16:07:20 +0100

    vault is good

--- src/app/backend/src/api/api_routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = apiRoutes;
-var chat_ts_1 = require("./chat.ts");
-var user_ts_1 = require("./user.ts");
-function apiRoutes() {
-    (0, chat_ts_1.default)();
-    (0, user_ts_1.default)();
-}
