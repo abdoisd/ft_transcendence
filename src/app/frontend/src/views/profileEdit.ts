eea237f8b1c40eb4389aec8bacbd672aba892692
Youness Lagmah <ylagmah@e3r11p4.1337.ma>
2025-09-26 02:29:20 +0100

    Update profile view

--- src/app/frontend/src/views/profileEdit.ts
+++ src/app/frontend/src/views/profileEdit.ts
@@ -68,21 +68,31 @@ async function EditUser(event: Event)
 window.EditUser = EditUser;
 
 const profileEditViewStaticPart = `
-<form id="edit-profile-form" onsubmit="EditUser(event)">
+	<div class="card center">
+		<h2 class="headline mv-5">Update Your Profile</h2>
+		<form id="edit-profile-form" onsubmit="EditUser(event)">
 
-  <label>
-    Username:
-    <input type="text" id="username" name="username">
-  </label>
-  <span id="username-error" style="color: red; display: none;"></span>
-  <br>
+			<div class="mb-3">
+				<label class="text-secondary">Username</label>
+				<div class="input">
+					<input type="text" id="username" name="username">
+				</div>
+			</div>
 
-  <label>
-    Avatar: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <input type="file" id="avatar" name="avatar" accept="image/*">
-  </label>
-  <br>
+			<span id="username-error" style="color: red; display: none;"></span>
 
-  <button type="submit">Save</button>
-</form>
+
+			<div class="mb-3">
+				<label class="text-secondary">Avatar</label>
+				<div class="input">
+					<input type="file" id="avatar" name="avatar" accept="image/*" class="file">
+				</div>
+			</div>
+
+			<button type="submit" class="btn btn-primary mt-6 w-full gap-small mb-3">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="white" width="24px" height="24px" viewBox="0 0 24 24"><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>
+				Save
+			</button>
+		</form>
+	</div>
 `;
